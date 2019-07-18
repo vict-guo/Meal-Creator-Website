db6bcf0bc26c5f9111b6360e8c6ddb94862bb3aa
vict-guo <victor.guo02@gmail.com>
2019-07-18 11:19:46 -0400

    adding initial files

---
+++ pages/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def home_view(request,*args, **kwargs):
+    return render(request, "home.html", {});
+
+def about_view(request,*args, **kwargs):
+    return render(request, "about.html", {});
+
+def contact_view(request,*args, **kwargs):
+    return render(request, "contact.html", {});
