2754eb2a97d03c6b72cea8f2c4946af2b97223c8
vict-guo <victor.guo02@gmail.com>
2019-08-11 11:36:22 -0400

    tuned checkboxes, restaurant api

--- static/main.js
+++ static/main.js
@@ -4,6 +4,7 @@ let cuisineID = new Map();
 let resTypeChoice = new Map();
 let resTypeID = new Map();
 var state = 0;
+var firstInList = true;
 
 $(document).ready(function() {
 
@@ -31,7 +32,7 @@ $(document).ready(function() {
     $("#nextBtn").click(function(){
         if(state == 1){
             state++;
-            console.log(resTypeID);
+            //console.log(resTypeID);
             if(resTypeID.size == 0){
                 findResType();
             }
@@ -39,11 +40,19 @@ $(document).ready(function() {
                 showResType();
             }
         }
+        else if(state == 2){
+            state++;
+            findRestaurant();
+        }
     });
     $("#prevBtn").click(function(){
         if(state == 2){
             state--;
             showCuisine();
+        }
+        if(state == 3){
+            state--;
+            showResType();
         }
     });
     function findCuisine(){
@@ -92,6 +101,45 @@ $(document).ready(function() {
             showResType();
         })
     }
+    function findRestaurant(){
+        url = 'https://developers.zomato.com/api/v2.1/search?';
+        url+= 'entity_id=';
+        url+= cityID.toString();
+        url+= '&entity_type=city&cuisines=';
+        firstInList = true;
+        for (let [a, b] of cuisineChoice) {
+            if(b && firstInList){
+                url+=a.toString();
+                firstInList = false;
+            }
+            else if(b){
+                url+='%2C';
+                url+=a.toString();
+            }
+        }
+        url+='&establishment_type=';
+        firstInList = true;
+        for (let [a, b] of resTypeChoice) {
+            if(b && firstInList){
+                url+=a.toString();
+                firstInList = false;
+            }
+            else if(b){
+                url+='%2C';
+                url+=a.toString();
+            }
+        }
+
+        fetch(url, {
+            headers:{
+                'user-key': '9ad0b763ba1a09e45d3c3eeb9e621a16'
+            }
+        }).then(function(response){
+            return response.json();
+        }).then(function(val){
+            console.log(JSON.stringify(val));
+        })
+    }
     function showCuisine(){
         $("#screen").html("");
         for (let [a, b] of cuisineID) {
@@ -105,21 +153,27 @@ $(document).ready(function() {
         }
     }
     var generateCuisineIcon = function(NAME){
-        var str = "";
+        var str = ""; var bool = cuisineChoice.get(cuisineID.get(NAME));
         str+='<div class="col-md-4"><div class="card mb-4 shadow-sm"><svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em"></text></svg>';
         str+='<div class="card-body"><p class="card-text">';
         str+=NAME;
-        str+='</p><div class="d-flex justify-content-between align-items-center"><div class="btn-group"><button type="button" class="btn btn btn-outline-secondary">View</button></div><div class="form-check"><input class="form-check-input" type="checkbox" value="" onchange=\'handleCuisine(this);\' id = \''+NAME+'\'></div>';
+        str+='</p><div class="d-flex justify-content-between align-items-center"><div class="btn-group"><button type="button" class="btn btn btn-outline-secondary">View</button></div><div class="form-check"><input class="form-check-input" type="checkbox" value="" onchange=\'handleCuisine(this);\' id = \''+NAME+'\'';
+        if(bool)
+            str+='checked';
+        str+='></div>';
         str+= '<small class="text-muted">Cuisine Type</small></div></div></div></div>';
         $("#screen").append(str);
-        //console.log(typeof(NAME));
+        //console.log(bool);
     }
     var generateResTypeIcon = function(NAME){
-        var str = "";
+        var str = ""; var bool = resTypeChoice.get(resTypeID.get(NAME));
         str+='<div class="col-md-4"><div class="card mb-4 shadow-sm"><svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em"></text></svg>';
         str+='<div class="card-body"><p class="card-text">';
         str+=NAME;
-        str+='</p><div class="d-flex justify-content-between align-items-center"><div class="btn-group"><button type="button" class="btn btn btn-outline-secondary">View</button></div><div class="form-check"><input class="form-check-input" type="checkbox" value="" onchange=\'handleResType(this);\' id = \''+NAME+'\'></div>';
+        str+='</p><div class="d-flex justify-content-between align-items-center"><div class="btn-group"><button type="button" class="btn btn btn-outline-secondary">View</button></div><div class="form-check"><input class="form-check-input" type="checkbox" value="" onchange=\'handleResType(this);\' id = \''+NAME+'\'';
+        if(bool)
+            str+='checked';
+        str+='></div>';
         str+= '<small class="text-muted">Restaurant Type</div></div></div>';
         $("#screen").append(str);
     };
