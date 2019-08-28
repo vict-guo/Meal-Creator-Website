7805865bc989dafa86b15a7d0da1b8b1f62aec7e
vict-guo <victor.guo02@gmail.com>
2019-08-28 09:38:49 -0400

    edited staticfiles

--- MealCreator/settings.py
+++ MealCreator/settings.py
@@ -126,6 +126,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
     '/var/www/static/',
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
